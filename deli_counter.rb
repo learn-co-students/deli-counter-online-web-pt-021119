f4e0480f16bfdacf959f150e695e6e108090eca1
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-05 17:37:18 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+describe 'Deli Counter' do
+  katz_deli = [0]
+  take_a_number(katz_deli, "there are no people on the line")
+
+  take_a_number(katz_deli, "The line is currently: 1. Logan 2. Avi 3. Spencer")
+
+  take_a_number(katz_deli, "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey")
+
+  describe "#take_a_number" do
+
+  take_a_number(katz_deli, "there is nobody in line")
+
+  take_a_number(katz_deli,"Welcome, Ada. You are number 1 in line.")
+  
